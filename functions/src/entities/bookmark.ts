be3877f1f0a1d438634c40ffa176478717207acf
himanoa <matsunoappy@gmail.com>
2019-04-12 17:17:36 +0900

    Implement bookmark parse function

--- functions/src/entities/bookmark.ts
+++ functions/src/entities/bookmark.ts
@@ -1,7 +1,18 @@
+import * as yup from "yup"
+
 interface Bookmark {
   id: string;
   url: string;
   bodyHtml: string;
-  comment: string;
+  comment: string | null;
 }
 
+export function parse(obj: Object): Bookmark {
+  const schema: yup.Schema<Bookmark> = yup.object().shape({
+    id: yup.string().required(),
+    url: yup.string().url(),
+    bodyHtml: yup.string(),
+    comment: yup.string().nullable()
+  })
+  return schema.validateSync(obj)
+}
