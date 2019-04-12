b8de35a83c93cb4c3bdaa3844a991c124eed7f35
himanoa <matsunoappy@gmail.com>
2019-04-12 16:17:17 +0900

    Implement test function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest(async (request, response) => {
+  try {
+    await admin.firestore().collection('test').add({test: 1})
+    response.sendStatus(200)
+  } catch (err) {
+    console.log(err)
+    response.sendStatus(400)
+  }
+});
