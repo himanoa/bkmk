21df07579669abb45413bde5cd05f32cec237fb9
himanoa <matsunoappy@gmail.com>
2019-04-14 10:32:52 +0900

    Fix

---
+++ frontend/src/main.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root: HTMLElement | null = document.getElementById("#root");
+  if (root) {
+    ReactDOM.render(<h1>HelloWorld</h1>, root);
+  }
+});
