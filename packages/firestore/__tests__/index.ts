9b76ec92142af06e76702aa2e0fb641ac5b769ed
himanoa <matsunoappy@gmail.com>
2019-04-24 15:08:37 +0900

    Fix firestore test
    Add typescript in dev dependencies

--- packages/firestore/__tests__/index.ts
+++ packages/firestore/__tests__/index.ts
@@ -1,3 +1,45 @@
+import * as fs from "fs"
+import * as path from "path"
+import {
+  initializeTestApp,
+  loadFirestoreRules,
+  clearFirestoreData,
+  assertFails,
+  assertSucceeds
+} from "@firebase/testing"
+
+import bookmarkFixture from "../utils/bookmark"
+
+const projectId= 'firestore-emulator-example-' + Date.now();
+
+const authorized = initializeTestApp({
+  projectId,
+  auth: { uid: "alice", email: "example@example.com" }
+}).firestore()
+
+const nonAuthorized = initializeTestApp({
+  projectId
+}).firestore()
+
+beforeAll(async() => {
+  await loadFirestoreRules({
+    projectId,
+    rules: fs.readFileSync(path.resolve(__dirname, "..", "firestore.rules"), "utf-8")
+  })
+})
+
+beforeEach(async () => {
+  await clearFirestoreData({
+    projectId
+  })
+})
+
+describe("bookmarks", () => {
+  it("should be create bookmark", async () => {
+    await assertSucceeds(authorized.collection("bookmarks").add(bookmarkFixture))
+  })
+})
+
 test("should be valid", () => {
-  expect(1).toBe(1)
+  expect(1).toBe(2)
 })
---
+++ packages/firestore/utils/bookmark.ts
@@ -0,0 +1,14 @@
+import * as faker from "faker"
+import * as firebase from "@firebase/testing"
+
+export default function(overwrite) {
+  return {
+    url: faker.internet.url(),
+    bodyHtml: faker.lorem.text(),
+    bodyText: faker.lorem.text(),
+    title: faker.lorem.sentence(),
+    comment: faker.lorem.paragraph(),
+    createdAt: firebase.firestore.Timestamp.now(),
+    updatedAt: firebase.firestore.Timestamp.now()
+  }
+}
