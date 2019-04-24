0f9e577bbca3393ae786b233eb5bda546de56fa3
himanoa <matsunoappy@gmail.com>
2019-04-24 15:38:28 +0900

    Refacor test

--- packages/firestore/__tests__/index.ts
+++ packages/firestore/__tests__/index.ts
@@ -1,53 +1,15 @@
-import * as fs from "fs"
-import * as path from "path"
 import {
-  initializeTestApp,
-  loadFirestoreRules,
-  clearFirestoreData,
   assertFails,
   assertSucceeds
 } from "@firebase/testing"
 
 import bookmarkFixture from "../utils/bookmark"
-
-const projectId = 'firestore-emulator-example-' + Date.now();
-
-const authorized = initializeTestApp({
-  projectId,
-  auth: { uid: "alice", email: "example@example.com" }
-})
-
-const nonAuthorized = initializeTestApp({
-  projectId
-})
-
-const authorizedStore = authorized.firestore()
-const nonAuthorizedStore = nonAuthorized.firestore()
-
-beforeAll(async(done) => {
-  await loadFirestoreRules({
-    projectId,
-    rules: fs.readFileSync(path.resolve(__dirname, "..", "firestore.rules"), "utf-8")
-  })
-  done()
-})
-
-afterAll(async () => {
-  return Promise.all([authorized.delete(), nonAuthorized.delete()])
-})
-
-beforeEach((done) => {
-  clearFirestoreData({
-    projectId
-  })
-  done()
-})
+import { authorizedStore, nonAuthorizedStore } from "../utils/store"
 
 describe("bookmarks", () => {
-  it("should be create bookmark", async (done) => {
+  it("should be create bookmark", async () => {
     await assertSucceeds(
       authorizedStore.collection("bookmarks").add(bookmarkFixture())
     )
-    done()
   })
 })
---
+++ packages/firestore/utils/store.ts
@@ -0,0 +1,40 @@
+import * as fs from "fs"
+import * as path from "path"
+import {
+  initializeTestApp,
+  loadFirestoreRules,
+  clearFirestoreData,
+} from "@firebase/testing"
+
+const projectId = 'firestore-emulator-example-' + Date.now();
+
+const authorized = initializeTestApp({
+  projectId,
+  auth: { uid: "alice", email: "example@example.com" }
+})
+
+const nonAuthorized = initializeTestApp({
+  projectId
+})
+
+beforeAll(async(done) => {
+  await loadFirestoreRules({
+    projectId,
+    rules: fs.readFileSync(path.resolve(__dirname, "..", "firestore.rules"), "utf-8")
+  })
+  done()
+})
+
+afterAll(async () => {
+  return Promise.all([authorized.delete(), nonAuthorized.delete()])
+})
+
+beforeEach((done) => {
+  clearFirestoreData({
+    projectId
+  })
+  done()
+})
+
+export const authorizedStore = authorized.firestore()
+export const nonAuthorizedStore = nonAuthorized.firestore()
