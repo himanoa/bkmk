1db47db0b70ae12352e5b201d11f1d9a0eb6b16f
himanoa <matsunoappy@gmail.com>
2019-05-06 01:22:10 +0900

    Implement BookmarkCommand

--- packages/browser/src/Bookmark/FirebaseBookmarkCommand.ts
+++ packages/browser/src/Bookmark/FirebaseBookmarkCommand.ts
@@ -1,19 +1,32 @@
 import { injectable, inject } from "inversify";
-import { BookmarkCommand, Bookmark } from "@bkmk/core";
+import { BookmarkCommand, Bookmark, BookmarkId } from "@bkmk/core";
 import * as firebase from "firebase/app";
 import "firebase/firestore";
+import { symbols } from "../Firebase/index";
+import omit from "lodash.omit";
 
 const documentKey = "bookmarks";
 @injectable()
 export class FirebaseBookmarkCommand implements BookmarkCommand {
-  constructor(public store: firebase.firestore.Firestore) {}
+  public store: firebase.firestore.Firestore;
+  constructor(@inject(symbols.firestore) store: firebase.firestore.Firestore) {
+    this.store = store;
+  }
 
   async create(bookmark: Bookmark) {
     this.store.collection(documentKey).add(bookmark);
   }
 
   async update(bookmark: Bookmark) {
-    this.store.collection(documentKey).doc(bookmark.id).update(bookmark);
+    this.store
+      .collection(documentKey)
+      .doc(bookmark.id)
+      .update(omit(bookmark, ["id"]));
   }
-  delete();
+  async delete(bookmarkId: BookmarkId) {
+    this.store
+      .collection(documentKey)
+      .doc(bookmarkId)
+      .delete();
+  }
 }
