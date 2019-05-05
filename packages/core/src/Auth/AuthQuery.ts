bb1a2d894b26a559ed4cee7fba842a81a1de3aca
himanoa <matsunoappy@gmail.com>
2019-05-05 17:09:13 +0900

    Fix prettier

--- packages/core/src/Auth/AuthQuery.ts
+++ packages/core/src/Auth/AuthQuery.ts
@@ -1,7 +1,7 @@
 import { Observable } from "rxjs";
 
 export abstract class AuthQuery {
-  public abstract isLogged(): Observable<boolean>
+  public abstract isLogged(): Observable<boolean>;
 }
 
 export default AuthQuery;
