bc72137cfa760c8e4af57f940c6d0ec5a0232a68
himanoa <matsunoappy@gmail.com>
2019-05-07 14:11:30 +0900

    Delete unused useEffect

--- packages/chrome-extension/src/index.tsx
+++ packages/chrome-extension/src/index.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useEffect } from "react";
 import * as ReactDOM from "react-dom";
 import "reflect-metadata";
 import { useInjection, InjectableProps } from "react-injection";
